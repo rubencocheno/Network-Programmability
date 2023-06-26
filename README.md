edb3857fba00e501e6b6410b45774c4d33d5aadd
rubencocheno <ruben@pxosys.com>
2023-06-26 16:25:56 +0100

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+## Network-Programmability
+
+This is part of teaching Module ACCA5029 (Network Programmability) at the University of Wales Trinity St. David, Institute of Inner City Learning London.
+
+These are the modules covered here on GitHub and practical examples:
+
+ Week 3 - Understanding and using API's
+
+ ### Tools
+
+In this module, you will install the DEVASC virtual machine (DEVASC VM) in Oracle VirtualBox. After completing
+the installation, you will explore the GUI interface. You will then create the necessary accounts for the
+services that you will use throughout the labs. 
+
+Required Resources
+• Host computer with at least 4 GB of RAM and 15 GB of free disk space
+• High-speed internet access to download Oracle VirtualBox and the DEVASC VM
+
+You can download the DEVASC OVA image and instructions below which contains all tools for this Module ACCA5029.
+
+https://drive.google.com/drive/folders/13gj7LW1phxE1DbucGTFew82NCDrBTytr?usp=sharing
---
+++ week3-ex1.py
@@ -0,0 +1,35 @@
+
+#Welcome to Week 3 of Network Programmability
+
+#This is a list of exercises to get some exposure to RESTAPI using Python.
+
+#Exercise 1
+#Get the Weather of London for today
+#Use the API Key under .nonsecrets file
+#Find Latitude and Longitude of London and execute a Get and dump JSON all details as output
+#Avoid any Loops
+#Check requirements and endpoints in https://openweathermap.org/current
+
+                
+#Exercise 2
+#Get the Weather of Great Malvern for today
+#Use the API Key under .nonsecrets file
+#Find City ID and execute a Get and dump JSON all details as output with temperature in Celsius
+#Avoid any Loops
+#Check requirements and endpoints in https://openweathermap.org/current
+
+
+#Exercise 3
+#Get the Weather for Lisbon, Portugal
+#Use the API Key under .nonsecrets file
+#Find the temperature in Celsius
+#Avoid any Loops
+#Check requirements and endpoints in https://openweathermap.org/current
+
+
+
+#Exercise 4
+#Compare all previous cities and return which one is the coldest and warmest by City (Temperature)
+#Use the API Key under .nonsecrets file
+
+#Check requirements and endpoints in https://openweathermap.org/current
